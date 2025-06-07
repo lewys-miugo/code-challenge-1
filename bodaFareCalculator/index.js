6a7874f1b969fac31b461119ad994de3fe12f75d
lewys-miugo <lewiswambugu01@gmail.com>
2025-06-07 07:51:41 +0300

    Adding comments and adding null validator

--- bodaFareCalculator/index.js
+++ bodaFareCalculator/index.js
@@ -1,17 +1,21 @@
 var baseFare = 50;
 var chargePerKm = 15;
+// This code calculates the fare for a boda boda ride based on the distance traveled.
 function calculateBodaFare(distance) {
     console.log("Welcome to Boda Fare Calculator!");
     console.log("Enter the distance in kilometers:");
 
     distance = parseInt(prompt("Distance (in km): "), 10);
 
-    if (distance <= 0 || isNaN(distance)) {
+    // Checking if the distance is valid
+    if (distance <= 0 || isNaN(distance) || distance === null) {
         return "Distance must be greater than zero.";
     } else {
+        // Calculating the total fare based on the distance
         let totalFare = baseFare + (distance * 15);
         console.log("The total fare for " + distance + " km is: Ksh " + totalFare);
     }
     
 }
+
 calculateBodaFare(0);
